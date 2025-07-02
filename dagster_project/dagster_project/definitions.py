4ede027590d916bfb66a14a69508d54ff5f9d0ea
SachinKhapre <khapresachin1010@gmail.com>
2025-07-02 19:55:41 +0530

    Added schedule to dagster project for dbt models

--- dagster_project/dagster_project/definitions.py
+++ dagster_project/dagster_project/definitions.py
@@ -2,12 +2,13 @@ from dagster import Definitions
 from dagster_dbt import DbtCliResource
 from .assets import dbt_cryptodata_dbt_assets
 from .project import dbt_cryptodata_project
-from .schedules import schedules
+from .schedules import run_all_dbt_assets_job, dbt_schedules
 
 defs = Definitions(
     assets=[dbt_cryptodata_dbt_assets],
-    schedules=schedules,
+    schedules=[dbt_schedules],
     resources={
         "dbt": DbtCliResource(project_dir=dbt_cryptodata_project),
     },
+    jobs=[run_all_dbt_assets_job],
 )
--- dagster_project/dagster_project/schedules.py
+++ dagster_project/dagster_project/schedules.py
@@ -2,14 +2,26 @@
 To add a daily schedule that materializes your dbt assets, uncomment the following lines.
 """
 from dagster_dbt import build_schedule_from_dbt_selection
-
+from dagster import ScheduleDefinition, define_asset_job
 from .assets import dbt_cryptodata_dbt_assets
 
-schedules = [
+#schedules = [
 #     build_schedule_from_dbt_selection(
 #         [dbt_cryptodata_dbt_assets],
 #         job_name="materialize_dbt_models",
 #         cron_schedule="0 0 * * *",
 #         dbt_select="fqn:*",
 #     ),
-]
+#]
+
+# Job to run all dbt assets
+run_all_dbt_assets_job = define_asset_job(
+    name="run_all_dbt_assets",
+    selection="*",
+)
+
+dbt_schedules = ScheduleDefinition(
+    job=run_all_dbt_assets_job,
+    cron_schedule="*/15 * * * *",
+    name="run_all_dbt_assets_schedule"
+)
